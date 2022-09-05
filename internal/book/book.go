2392cecf2c17b009f19ef0a08359d5393e2af8a6
Chris Carpenter <cdcarpenter@gmail.com>
2022-09-05 11:00:19 -0500

    Add books template

--- internal/book/book.go
+++ internal/book/book.go
@@ -2,7 +2,11 @@ package book
 
 import (
 	"fmt"
+	"sort"
+	"time"
 )
+
+const layout = "2006/01/02"
 
 type Book struct {
 	Name        string            `json:"name"`
@@ -15,4 +19,38 @@ type Book struct {
 
 func (b Book) ToMarkdownLink() string {
 	return fmt.Sprintf("[%s](%s)[A](%s)", b.Name, b.Links["website"], b.Links["amazon"])
+}
+
+func (b Book) NextReleaseDate() string {
+	if b.ReleaseDate == "" && b.AudioDate == "" {
+		return "Unknown"
+	}
+	var dates []time.Time
+	today := time.Now()
+	releaseDate, err := time.Parse(layout, b.ReleaseDate)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return "err"
+	}
+	if today.Before(releaseDate) {
+		dates = append(dates, releaseDate)
+	}
+	audioDate, err := time.Parse(layout, b.AudioDate)
+	if err != nil {
+		fmt.Printf("Error: %s", err.Error())
+		return "err"
+	}
+	if today.Before(audioDate) {
+		dates = append(dates, audioDate)
+	}
+	sort.Slice(dates, func(i, j int) bool {
+		return dates[i].Before(dates[j])
+	})
+	if b.ReleaseDate == "" && len(dates) == 0 {
+		return "Release Unknown"
+	}
+	if b.AudioDate == "" && len(dates) == 0 {
+		return "Audiobook Unknown"
+	}
+	return "Unknown"
 }
